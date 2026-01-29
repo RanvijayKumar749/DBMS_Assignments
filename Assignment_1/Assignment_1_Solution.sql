aa9e1fddf308480742051c4e719cc2549047c7d9
Ranvijay Kumar <ranvijay1312kumar@gmail.com>
2026-01-29 23:27:46 +0530

    Assignment_1_Solution.sql

--- Assignment_1/Assignment_1_Solution.sql
+++ Assignment_1/Assignment_1_Solution.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Student (
     RollNo INT,
-    Name VARCHAR(50),
-    Dept VARCHAR(20),
+    Name VARCHAR2(50),
+    Dept VARCHAR2(20),
     Age INT,
-    Phone VARCHAR(15)
+    Phone VARCHAR2(15)
 );
 
 CREATE TABLE Course (
     CourseID INT,
-    CourseName VARCHAR(50),
+    CourseName VARCHAR2(50),
     Credits INT
 );
 
-ALTER TABLE Student ADD City VARCHAR(30);
+ALTER TABLE Student ADD City VARCHAR2(30);
 ALTER TABLE Student ADD Semester INT;
 
 ALTER TABLE Student RENAME COLUMN Phone TO MobileNo;
@@ -22,10 +22,18 @@ DROP TABLE Course;
 
 
 INSERT INTO Student VALUES
-(101, 'Rahul', 'CSE', 21, '9876543210', 'Delhi', 5),
-(102, 'Aman', 'CSE', 22, '9876543222', 'Patna', 6),
-(103, 'Neha', 'ECE', 20, '9876543333', 'Ranchi', 4),
-(104, 'Pooja', 'ME', 23, '9876543444', 'Kolkata', 7),
+(101, 'Rahul', 'CSE', 21, '9876543210', 'Delhi', 5);
+
+INSERT INTO Student VALUES
+(102, 'Aman', 'CSE', 22, '9876543222', 'Patna', 6);
+
+INSERT INTO Student VALUES
+(103, 'Neha', 'ECE', 20, '9876543333', 'Ranchi', 4);
+
+INSERT INTO Student VALUES
+(104, 'Pooja', 'ME', 23, '9876543444', 'Kolkata', 7);
+
+INSERT INTO Student VALUES
 (105, 'Ravi', 'CSE', 19, '9876543555', 'Patna', 3);
 
 SELECT * FROM Student;
